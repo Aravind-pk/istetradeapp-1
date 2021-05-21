a0a058be8e4241a49f9a9676e282f9625cf0e687
webdesign4alll <asbupsilonviroidno1@gmail.com>
2021-05-21 11:48:13 +0530

    styles responsive 80%;

---
+++ src/components/LogOutM.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import axios from "axios";
+
+import { history } from "../router/Approuter";
+
+export default class LogOut extends React.Component<any> {
+
+    handleClick = () => {
+        axios.delete(`${process.env.REACT_APP_BACKEND_URL}/users/logout`, {headers: {
+            "x-auth": sessionStorage.getItem("JWT_Token")
+        }}).then(() => {
+            sessionStorage.removeItem("JWT_Token")
+            sessionStorage.removeItem("User_ID")
+            sessionStorage.removeItem("name")
+            sessionStorage.removeItem("status")
+            history.push("/login")
+        })
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick} className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                Log Out
+            </button>
+        )
+    }
+}
